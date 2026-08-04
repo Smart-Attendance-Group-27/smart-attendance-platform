6bcfab1ce2887cad9e37ce3306d7c000ec0da055
Oshadha <oshadhawijayarathne63@gmail.com>
2026-08-05 00:43:57 +0530

    feat(mobile): add student profile type

---
+++ apps/mobile/src/features/profile/types/profile.types.ts
@@ -0,0 +1,7 @@
+export type StudentProfile = {
+  readonly id: string;
+  readonly registrationNumber: string;
+  readonly fullName: string;
+  readonly universityEmail: string;
+  readonly profileImageUrl?: string;
+};
